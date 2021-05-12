82ad51a5318d68890f3d4592176f41e9b1b88cd3
JadallahZaytoon <jadallah.zaytoon@gmail.com>
2021-05-12 23:41:03 +0300

    Pulse check finished

--- app.js
+++ app.js
@@ -3,6 +3,43 @@ const app = express();
 const PORT = 3000;
 
 
+const users = ["John", "Mark"];
+
+
+app.use(express.json());
+
+app.get("./",(req,res)=>{
+
+    console.log("Hello");
+
+});
+
+const logUsers = (req,res,next)=>{
+    console.log(users);
+    next();
+};
+
+
+
+const logMethod=(req,res,next)=>{
+    console.log(req.method);
+    next();
+};
+
+app.use("/users",logMethod);
+
+
+app.get("/users", (req, res, next) => {
+    res.json(users);
+  });
+
+  app.get("/",(req,res,next)=>{
+
+    const err= new Error("Error server");
+    err.status=500;
+    next(err);
+  })
+  
 
 app.listen(PORT,(req,res)=>{
     console.log(`Working on ${PORT}`);
